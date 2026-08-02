e7330ef9bf0b752d97bf3f0e78c44d2dad09f4e2
Youssef Mohamed <youssefmohamed.flutter@gmail.com>
2026-08-02 20:21:33 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          children: [
+            Image.asset(
+              'assets/images/birthdayCard.jpg',
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+            ),
+            Center(
+              child: Text(
+                textAlign: TextAlign.center,
+                'Happy Birthday',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                  shadows: [
+                    Shadow(
+                      blurRadius: 15,
+                      color: Colors.black.withOpacity(0.6),
+                      offset: const Offset(4, 4),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
